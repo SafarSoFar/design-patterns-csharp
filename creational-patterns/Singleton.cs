639443dc2720352130da0e9a32d61e6888c1fa57
Isaev Safar <133248217+SafarSoFar@users.noreply.github.com>
2024-08-05 19:23:07 +0500

    Update Singleton.cs

--- creational-patterns/Singleton.cs
+++ creational-patterns/Singleton.cs
@@ -5,9 +5,10 @@
 // because inheritance can violate the singleton principle
 public sealed class Singleton{
     public int val = 0; // a demonstrative field
-    private static Singleton instance;
-    public static Singleton Instance{
+    private static Singleton instance; //private field
+    public static Singleton Instance{ // instance property
         get{
+            // Lazy initialization. Initialization when the instance is required for the first time
             if(instance == null){
                 instance = new Singleton();
             }
@@ -18,16 +19,34 @@ public sealed class Singleton{
 
     
 }
-//---------------------------------------------------------------------------------------------------
+//------------------------------------THREAD SAFE------------------------------------
 
+//By using lock we can ensure that different threads cannot read/write at the same time 
 public sealed class ThreadSafeSingleton{
-    public int val = 0;
+    public int val = 0; // a demonstrative field
+    private static ThreadSafeSingleton instance; // private field
+    public static readonly object padlock = new object(); // lock object
+    public static ThreadSafeSingleton Instance{ // instance property
+        get{    
+            lock(padlock){
+
+                // Lazy initialization. Initialization when the instance is required for the first time
+                if(instance == null){
+                    instance = new ThreadSafeSingleton();
+                }
+                return instance;    
+
+            }
+        }
+        private set{} // to prevent creating through the class constructor 
+    }
 }
+//----------------------------------THREAD-SAFE------------------------------------------
 
 class Program(){
     public static void Main(string[] args){
 
-//-------------------NOT THREAD-SAFE-----------------------------------------------------------------
+//-------------------NOT THREAD-SAFE----------------------------------------------------------------
 
         //SingletonClass.Instance = new SingletonClass(); - doens't compile due to inaccessibility
 
@@ -35,6 +54,15 @@ class Program(){
         Singleton.Instance.val = 15; 
         Console.WriteLine(Singleton.Instance.val); // 15
 
+//--------------------------------------------------------------------------------------------------
+//------------------THREAD-SAFE---------------------------------------------------------------------
+        
+        //ThreadSafeSingleton.Instance = new ThreadSafeSingleton(); - doesn't compile due to inaccessibility
+
+        Console.WriteLine(ThreadSafeSingleton.Instance.val); // 0
+        ThreadSafeSingleton.Instance.val = 15;
+        Console.WriteLine(ThreadSafeSingleton.Instance.val); // 15
+        
 //--------------------------------------------------------------------------------------------------
     }
 }
